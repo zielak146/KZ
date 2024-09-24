f0e615e2249201a2f71ff1d2fb0f17738f7d3918
zielak146 <125725768+zielak146@users.noreply.github.com>
2024-09-24 12:31:29 +0100

    Upload

---
+++ main.js
@@ -0,0 +1,126 @@
+const navItems = document.querySelectorAll('.nav_item');
+const navBtn = document.querySelector('.burger-btn');
+const navBar = document.querySelector('.nav');
+const arrowUp = document.querySelector('.arrow-up');
+
+const handleNav = () => {
+	navBar.classList.toggle('active');
+	navItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			navBar.classList.remove('active');
+		});
+	});
+	handleNavAnimation();
+};
+
+navBtn.addEventListener('click', handleNav);
+
+const handleNavAnimation = () => {
+	let time = `0`;
+	navItems.forEach((item) => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = `.${time}s`;
+		time++;
+	});
+};
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} else {
+			entry.target.classList.remove('show');
+		}
+	});
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+const btn = document.querySelector('.btn');
+
+const btnAnimation = (e) => {
+	const y = e.clientY;
+	const x = e.clientX;
+
+	const innerY = e.target.offsetTop;
+	const innerX = e.target.offsetLeft;
+
+	const top = y - innerY;
+	const left = x - innerX;
+	const circle = document.createElement('span');
+	circle.classList.add('circle');
+	circle.style.top = `${top}px`;
+	circle.style.left = `${left}px`;
+	e.target.appendChild(circle);
+};
+
+btn.addEventListener('click', btnAnimation);
+
+const headerHeading = document.querySelector('.header-heading');
+let inputValue = `Websites tailored to your business. `;
+let timeout;
+let index = 0;
+let speed = 70;
+let startDelay = 700;
+
+const writingAnimation = () => {
+	headerHeading.innerHTML = inputValue.slice(0, index);
+	headerHeading.classList.add('fade-in');
+	index++;
+	if (index > inputValue.length) return;
+	const randomSpeed = speed + Math.random() * 100;
+	setTimeout(writingAnimation, randomSpeed);
+};
+
+const addCursor = () => {
+	const cursor = document.createElement('span');
+	cursor.classList.add('cursor');
+	headerHeading.appendChild(cursor);
+  };
+  
+  // Start typing with a delay
+  setTimeout(() => {
+	addCursor(); // Add the cursor before starting typing
+	writingAnimation();
+  }, startDelay);
+
+
+
+  //ACCORDION
+  const accordion = document.querySelector('.about-us__accordion')
+const accordionBtns = document.querySelectorAll('.about-us__accordion-btn')
+
+function openAccordionItems() {
+
+	if(this.nextElementSibling.classList.contains('active')) {
+		this.nextElementSibling.classList.remove('active')
+	} else {
+
+		closeAccordionItem()
+		this.nextElementSibling.classList.toggle('active')
+		
+	}
+	
+}
+
+const closeAccordionItem = () => {
+	const allActiveItems = document.querySelectorAll('.about-us__accordion-info')
+	allActiveItems.forEach(item => item.classList.remove('active'))
+}
+
+const clickOutsideAccordion = e => {
+    if (
+		e.target.classList.contains('about-us__accordion-btn') ||
+		e.target.classList.contains('about-us__accordion-info') ||
+		e.target.classList.contains('about-us__accordion-info-text')
+	)
+		return
+
+	closeAccordionItem()
+}
+
+accordionBtns.forEach(btn => btn.addEventListener('click', openAccordionItems))
+
+window.addEventListener('click', clickOutsideAccordion)
